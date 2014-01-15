c893be8350dba8bf5634234e4ccd6b5ace426840
lepercon <uscart@gmail.com>
2014-01-14 21:24:20 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * IMREE - PHP
+ * 
+ * Project Description et al...
+ * 
+ * 
+ * 
+ * SETUP
+ * ========
+ * 
+ * Setup your MySQL database.
+ * @todo make mysql setup.sql
+ * 
+ * setup your config file and point to it:
+ */
+
+$imree_core_config_path = "../config.php";
+
+
+
+/**
+ * 
+ * Setup complete. run index.php from your browser
+ * 
+ * 
+ * ---------------------------------------------------------
+ * The code below here should not be edit by end users.
+ */
+
+
+
+//@todo make initilizing function
+//imree.do();
+
