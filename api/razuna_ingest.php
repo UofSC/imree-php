142316bed63eb04853b1540480380b6ea2297e4a
Ramon Tavares <rbwjazzflavor@gmail.com>
2014-02-06 16:51:34 -0500

    retrieve records from Razuna server
    wrote clunky code to retrieve records from Razuna functions come later?

--- api/razuna_ingest.php
+++ api/razuna_ingest.php
@@ -25,15 +25,70 @@ require_once('/../shared_functions/functions.form.php');
  global $search;       
 //0. Function Display Form
 
+ $target = htmlspecialchars($_SERVER["PHP_SELF"]);
+ $search_limit = array("img" => "image only", "vid" => "video only","doc" => "document only","aud" => "audio only");
+ #$query_string="";
+ 
     if(form_submitted())
     {//form has been submitted perform the appropriate checks 
-    
+        //build the file url from the search query
+        $base="http://imree.tcl.sc.edu:8080/razuna/global/api2/search.cfc?method=searchassets";
+        $api="&api_key=822756B3669444D59D2C2333E449FFBA";
+        $q_string= "&searchfor=".$_POST["query_string"];       
         
+        if (!isset($_POST["show_ass"])) //check to see if any search limiters have been set and add them to the file url
+        {
+            $show_ass="";
+        }
+        else
+        {
+            $show_ass="&show=".$_POST["show_ass"];
+        }
+        
+        $url =  $base.$api.$q_string.$show_ass; //create the url to pass to the "function"
+        
+        echo $_POST["query_string"]; //testing ps
+        print "\n<br>";
+        
+        if (isset($_POST["show_ass"])) //testing ps
+        {   echo $_POST["show_ass"];
+        }
+        else {
+            echo "O show_ass is a no show.";
+        }
+        
+        print "\n<br>";
+        echo $url; //testing ps
+        
+        $contents = file_get_contents($url); //get the contents of the search from razuna
+        $contents = utf8_encode($contents);  //encode them
+        $results = json_decode($contents,true); //pass the results to json for nifty array handling
+        print "\n<br>";
+        print_r ($results); //testing ps
+        
+        //to do
+        //print records in a human readable form
+        //select records
+        //ingest records to IMREE DB
     } 
      else 
-    {//form has not been submitted
-      //Create an option to show an advanced search
-        if ($search == "ADV") //not sure if I will implement advanced and simple search ... talk to JSON about this
+    {   //form has not been submitted
+        //create a input field to type the search query
+        echo "<form method='post' action='$target'>";
+        echo "Simple Search";
+        print "\n";
+        f_input("query_string","text","");
+        print "\n<br>";
+        echo "Limit Search";
+        print "\n";
+        f_input("show_ass","radio", $search_limit);
+        print "\n(Default search is all types)";
+        print "\n<br>";
+        f_input("SUMBIT","submit");
+        echo "</form>";
+      
+//Create an option to show an advanced search
+      /*  if ($search == "ADV") //not sure if I will implement advanced and simple search ... talk to JSON about this
         {
 
         }
@@ -41,7 +96,7 @@ require_once('/../shared_functions/functions.form.php');
         //Default show the user the simple search
         {
 
-        }
+        }*/
     }
     
     
