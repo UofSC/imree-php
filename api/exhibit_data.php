3baf3279a1a5853734fb3ba65683fc86e178bf52
jasonsteelman <uscart@gmail.com>
2014-03-07 15:58:22 -0500

    ORDER BY asset.order

--- api/exhibit_data.php
+++ api/exhibit_data.php
@@ -4,7 +4,7 @@ function exhibit_data($exhibit_id) {
 	$conn = db_connect();
 	$exhibit_results = db_query($conn, "SELECT * FROM exhibits WHERE exhibit_id = ".  db_escape($exhibit_id));
 	$exhibit['exhibit_properties'] = $exhibit_results[0];
-	$results = db_query($conn, "SELECT * FROM modules WHERE exhibit_id = ".  db_escape($exhibit_id) . " AND module_parent_id = 0");
+	$results = db_query($conn, "SELECT * FROM modules WHERE exhibit_id = ".  db_escape($exhibit_id) . " AND module_parent_id = 0 ORDER BY module_order ASC ");
 	for($i = 0; $i< count($results); $i++) {
 		$child_modules = exhibit_child_modules($results[$i]['module_id']);
 		if($child_modules) {
@@ -24,7 +24,7 @@ function exhibit_data($exhibit_id) {
 function exhibit_module_assets($module_id) {
 	global $imree_absolute_path;
 	$conn = db_connect();
-	$assets = db_query($conn, "SELECT module_assets.*, asset_data.asset_data_type AS module_type, asset_data.asset_data_name, 1 AS asset FROM module_assets LEFT JOIN asset_data USING (asset_data_id) WHERE module_id = ".db_escape($module_id));
+	$assets = db_query($conn, "SELECT module_assets.*, asset_data.asset_data_type AS module_type, asset_data.asset_data_name, 1 AS asset FROM module_assets LEFT JOIN asset_data USING (asset_data_id) WHERE module_id = ".db_escape($module_id). " ORDER BY module_asset_order ASC");
 	if(count($assets)) {
 		for($i =0; $i < count($assets); $i++) {
 			if($assets[$i]['asset_data_id'] > 0) {
@@ -47,7 +47,7 @@ function exhibit_module_assets($module_id) {
 
 function exhibit_child_modules($module_parent_id) {
 	$conn = db_connect();
-	$children = db_query($conn, "SELECT *, 0 AS asset FROM modules WHERE module_parent_id = ".  db_escape($module_parent_id));
+	$children = db_query($conn, "SELECT *, 0 AS asset FROM modules WHERE module_parent_id = ".  db_escape($module_parent_id) . " ORDER BY module_order ASC");
 	if(count($children)) {
 		for($i = 0; $i < count($children); $i++) {
 			$grandkids = exhibit_child_modules($children[$i]['module_id']);
