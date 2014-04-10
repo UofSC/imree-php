8695767338057700c6a57ca05eae37071919df05
jasonsteelman <uscart@gmail.com>
2014-04-10 12:52:02 -0400

    Adding group methods

--- imree_functions/imree.group.php
+++ imree_functions/imree.group.php
@@ -1,32 +1,16 @@
 <?php
 
 
-class imree_group {
-    public $group_name;
-    public $children;
-    public $type;
-    
-    public function __construct($id = null) {
-        if($id) {
-            $this->load($id);
-        }
-    }
-    
-    public function load($id) {
-        //get group data from database where row_id = $id
-    }
-    
-    public function add_child($child) {
-        //update stack and save()
-    }
-    
-    public function remove_child($child) {
-        //update stack and save()
-    }
-    
-    public function save() {
-        //save groupdata to database
-        //save each child's update data (if the order changed, etc...
-    }
-    
+function imree_group_new($people_group_name, $creator_id) {
+	$conn = db_connect();
+	$results = db_exec($conn, build_insert_query($conn, "people_group", array(
+	    'people_group_name' => $people_group_name,
+	    'people_group_creator' => $creator_id,
+	    'people_group_created' => date("Y-m-d H:i:s"),
+	)));
+	if($results) {
+		return $results['last_id'];
+	} else {
+		return false;
+	}
 }
